57ec94e683133e6173ac2d96f14471ca8436889f
Latriel <Selenian@yandex.ru>
2024-04-20 19:02:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/header/header";
 // window.Telegram.WebApp.
 // const tg = window.Telegram.WebApp;
 
@@ -18,6 +19,7 @@ function App() {
 
   return (
     <div className="App">
+        <Header/>
 <button onClick={onToggleButton}>does it work?</button>
         {/*<button onClick={onClose}>Закрыть</button>*/}
     </div>
