74d6f4ce26c0ff4580f4e95268866beeac4f085d
Latriel <Selenian@yandex.ru>
2024-04-21 17:51:02 +0300

    init

--- src/form/form.jsx
+++ src/form/form.jsx
@@ -4,9 +4,9 @@ import {useTelegram} from "../hooks/useTelegram";
 
 
 
-const Form = ()=> {
-    const [country, setCountry] = useState();
-    const [street, setStreet] = useState();
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram()
 
@@ -14,7 +14,7 @@ const Form = ()=> {
             const data = {
                 country,
                 street,
-                subject
+                subject,
             }
             tg.sendData(JSON.stringify(data))
         }, [], );
